d31a3362337963c0257e39cc824961cbf9a67a94
Meenakshi Sundaram <vms20591@gmail.com>
2018-02-19 00:48:49 +0530

    The match Control Flow Operator

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
 fn main() {
-    // Compiler infer type from "Some" because value is present
-    let some_number = Some(1);
-    let some_string = Some("string");
+    value_in_coins(Coin::Penny);
+}
 
-    // Compiler can't infer type because value is absent
-    let absent_number: Option<i32> = None;
-
-    let a: i8 = 5;
-    let b: Option<i8> = Some(5);
-
-    //let c = a + b; // Compile error
-                    // Compiler ensures we have "value" before using it elsewhere
-                    // Need to convert Option<T> to <T> before using <T> elsewhere
-
-    // When we use something that isn't Option<T>, we can be confident value isn't null
-    // When we use Option<T>, compiler forces us to handle case of value being null
+fn value_in_coins(coin: Coin) -> u32 {
+    // "match" - keyword
+    // "coin" - expression (can return any value)
+    // "{ sts }" - arm => LHS - pattern, RHS - expression
+    // patterns searched in order and stop at matching one
+    match coin {
+        Coin::Penny => {
+            println!("Lucy Penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
